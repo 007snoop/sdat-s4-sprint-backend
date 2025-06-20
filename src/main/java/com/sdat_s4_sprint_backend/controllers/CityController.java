9ce0d581919815bc1817af87a20a9166dcd3c5eb
Colin Yetman <colinguitarist@gmail.com>
2025-06-20 15:05:42 -0230

    city should be complete, might need updates later

--- src/main/java/com/sdat_s4_sprint_backend/controllers/CityController.java
+++ src/main/java/com/sdat_s4_sprint_backend/controllers/CityController.java
@@ -1,4 +1,40 @@
 package com.sdat_s4_sprint_backend.controllers;
 
+import com.sdat_s4_sprint_backend.entity.City;
+import com.sdat_s4_sprint_backend.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
 public class CityController {
+    @Autowired
+    private CityService cityServ;
+
+    @GetMapping
+    public List<City> getAllCities() {
+        return cityServ.getAllCities();
+    }
+
+    @GetMapping("/{id}")
+    public City getCity(@PathVariable Long id) {
+        return cityServ.getCity(id);
+    }
+
+    @PostMapping
+    public City addCity(@RequestBody City city) {
+        return cityServ.addCity(city);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCity(@PathVariable Long id) {
+        cityServ.deleteCity(id);
+    }
+
+    public List<?> getAirportsForCity(@PathVariable Long id) {
+        City city = cityServ.getCity(id);
+        return city != null ? city.getAirports() : List.of();
+    }
 }

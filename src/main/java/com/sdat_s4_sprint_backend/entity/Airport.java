10dd2c10ad8828aca970330f6dfaa20d771bee7b
Colin Yetman <colinguitarist@gmail.com>
2025-06-22 15:06:30 -0230

    airport entity update

--- src/main/java/com/sdat_s4_sprint_backend/entity/Airport.java
+++ src/main/java/com/sdat_s4_sprint_backend/entity/Airport.java
@@ -1,4 +1,48 @@
 package com.sdat_s4_sprint_backend.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Airport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String portId;
+    @ManyToOne
+    @JoinColumn(name = "city_id") // this sets the foreign key restraint
+    private City city;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPortId() {
+        return portId;
+    }
+
+    public void setPortId(String portId) {
+        this.portId = portId;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
 }

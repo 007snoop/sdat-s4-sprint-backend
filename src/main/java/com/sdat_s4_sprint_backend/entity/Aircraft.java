2b2631251eec3004ae615e5fb87da11e6cde85d4
Colin Yetman <colinguitarist@gmail.com>
2025-06-23 13:12:03 -0230

    ac repo entity deploy update

--- src/main/java/com/sdat_s4_sprint_backend/entity/Aircraft.java
+++ src/main/java/com/sdat_s4_sprint_backend/entity/Aircraft.java
@@ -1,4 +1,11 @@
 package com.sdat_s4_sprint_backend.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Aircraft {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }

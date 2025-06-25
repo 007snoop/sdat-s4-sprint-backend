76a85823a5cdd28e3295938f479c693a887f5d24
Colin Yetman <colinguitarist@gmail.com>
2025-06-25 11:46:01 -0230

    handle recursive data

--- src/main/java/com/sdat_s4_sprint_backend/service/AirportService.java
+++ src/main/java/com/sdat_s4_sprint_backend/service/AirportService.java
@@ -6,8 +6,6 @@ import com.sdat_s4_sprint_backend.repos.AirportRepository;
 import com.sdat_s4_sprint_backend.repos.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -18,46 +16,46 @@ public class AirportService {
     private CityRepository cityRepo;
 
     @Autowired
-    private AirportRepository AirportRepo;
+    private AirportRepository airportRepository;
 
     public List<Airport> getAllAirports() {
-        return AirportRepo.findAll();
+        return airportRepository.findAll();
     }
 
     public Airport getAirport(Long id) {
-        return AirportRepo.findById(id).orElse(null);
+        return airportRepository.findById(id).orElse(null);
     }
 
     public Airport addAirport(Airport ap, Long cityId) {
         City city = cityRepo.findById(cityId).orElse(null);
         if (city != null) {
             ap.setCity(city);
-            return AirportRepo.save(ap);
+            return airportRepository.save(ap);
         } else {
             return null;
         }
     }
 
     public void deleteAirport(Long id) {
-        AirportRepo.deleteById(id);
+        airportRepository.deleteById(id);
     }
 
     public Airport updateAirport(Long id, Airport a) {
-        Airport e = AirportRepo.findById(id).orElse(null);
+        Airport e = airportRepository.findById(id).orElse(null);
         if (e != null) {
             e.setName(a.getName());
             e.setPortId(a.getPortId());
-            return AirportRepo.save(e);
+            return airportRepository.save(e);
         } return null;
     }
 
     public Airport patchAirport(Long id, Airport p) {
-        Airport e = AirportRepo.findById(id).orElse(null);
+        Airport e = airportRepository.findById(id).orElse(null);
         if (e != null) {
             if (p.getName() != null) e.setName(p.getName());
             if (p.getCity() != null) e.setCity(p.getCity());
             if (p.getPortId() != null) e.setPortId(p.getPortId());
-            return AirportRepo.save(e);
+            return airportRepository.save(e);
         } return null;
     }
 }
